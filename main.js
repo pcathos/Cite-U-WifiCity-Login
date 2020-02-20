b333e1df1a880d182ae139df9400ab1baf4312c4
pcathos <33541685+pcathos@users.noreply.github.com>
2020-02-20 22:35:06 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Cite U wifi-city login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://10.254.0.254:1000/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.getElementById('ft_un').value = "nee_606";
+    document.getElementById('ft_pd').value = "EH9P64fW";
+    console.log("working");
+    document.getElementsByTagName('input')[5].click();
+
+    function closeSite(){
+        if (navigator.userAgent.indexOf("MSIE") > 0) {
+            if (navigator.userAgent.indexOf("MSIE 6.0") > 0) {
+                window.opener = null;
+                window.close();
+            } else {
+                window.open('', '_top');
+                window.top.close();
+            }
+        }
+        else if (navigator.userAgent.indexOf("Firefox") > 0) {
+            window.location.href = 'about:blank ';
+        } else {
+            window.opener = null;
+            window.open('', '_self', '');
+            window.close();
+        }
+    }
+    setTimeout(closeSite(),2000);
+})();
